7d25ccc00e14968922e20b60c529f41ff70b0810
Ubuntu <kusha@cli-machine.rlpnsp2ivwku5beb02l5tjrmsf.zx.internal.cloudapp.net>
2021-06-20 14:16:56 +0000

    Updated.

--- apply.sh
+++ apply.sh
@@ -2,6 +2,6 @@
 kubectl apply -f db-deployment.yaml
 sleep 10
 kubectl apply -f app-deployment.yaml
-sleep 10
+sleep 15
 kubectl exec $(kubectl get pods | grep "app") -- python3 create.py
 kubectl get services | grep "app-service"
